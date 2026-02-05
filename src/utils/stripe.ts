ea5c7a90ea04ca273d943d5199ac81f3b0b77cbd
Muhammad Zain <zain683711@gmail.com>
2026-02-05 17:13:17 +0500

    fix: update Stripe API version to 2025-10-29.clover

--- src/utils/stripe.ts
+++ src/utils/stripe.ts
@@ -2,5 +2,5 @@ import { Stripe} from "stripe";
 
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: "2025-09-30.clover",
+    apiVersion: "2025-10-29.clover",
 })
